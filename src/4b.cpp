d438d6f7a32cb0379c9289953d307034e8f19364
Steven Seeger <steven.seeger@flightsystems.net>
2022-12-09 09:34:53 -0500

    simplify 4b. way overthought it. thanks stephen

--- src/4b.cpp
+++ src/4b.cpp
@@ -12,14 +12,7 @@ int main()
         char dummy;
         istr >> vals[0] >> dummy >> vals[1] >> dummy >> vals[2] >> dummy >> vals[3];
 
-        auto f = [](int32_t v, int32_t l, int32_t r) {
-            return ((v>=l)&&(v<=r));
-        };
-
-        if(f(vals[0], vals[2], vals[3])) ++total;
-        else if(f(vals[1], vals[2], vals[3])) ++total;
-        else if(f(vals[2], vals[0], vals[1])) ++total;
-        else if(f(vals[3], vals[0], vals[1])) ++total;
+        if((vals[0] <= vals[3]) && (vals[1] >= vals[2])) ++total;
     });
 
     std::cout << total << std::endl;
